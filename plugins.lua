2a02083c6a36efb2e11477577e30ba0621a5af48
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-06-28 01:41:05 +0330

    Modify neo-tree to close on open file action

--- plugins.lua
+++ plugins.lua
@@ -12,6 +12,15 @@ local surround = {
   config = function() require("nvim-surround").setup({}) end
 }
 
+local neotree = {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = function(_, opts)
+    local close_on_open_handler = { event = "file_opened", handler = function(_) require("neo-tree").close_all() end }
+    table.insert(opts.event_handlers, close_on_open_handler)
+    return opts
+  end
+}
+
 local notify = { "rcarriga/nvim-notify", config = require("user.configs.notify").config, lazy = false }
 local neoscroll = { "karb94/neoscroll.nvim", config = require("user.configs.neoscroll").config, lazy = false }
 local hairline = { "rebelot/heirline.nvim", opts = require("user.configs.heirline").opts }
@@ -38,4 +47,5 @@ return {
   surround,
   notify,
   neoscroll,
+  neotree,
 }
