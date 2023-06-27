3af341633bf28f4cd4df863b2c6616014b590fa2
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-06-27 22:38:02 +0330

    Modify hop commands to be multiline

--- configs/hop.lua
+++ configs/hop.lua
@@ -8,20 +8,20 @@ M.config = function() hop().setup({ keys = "asdfghjkl;weruionmcv" }) end
 
 M.keys.char_line_after = function()
   hop().hint_char1({
-    direction = direction().AFTER_CURSOR, current_line_only = true })
+    direction = direction().AFTER_CURSOR })
 end
 
 M.keys.char_line_before = function()
   hop().hint_char1({
-    direction = direction().BEFORE_CURSOR, current_line_only = true })
+    direction = direction().BEFORE_CURSOR })
 end
 
 M.keys.char_line_after_till = function()
-  hop().hint_char1({ direction = direction().AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+  hop().hint_char1({ direction = direction().AFTER_CURSOR, hint_offset = -1 })
 end
 
 M.keys.char_line_before_till = function()
-  hop().hint_char1({ direction = direction().BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+  hop().hint_char1({ direction = direction().BEFORE_CURSOR, hint_offset = 1 })
 end
 
 
