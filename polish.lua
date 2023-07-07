9b09eeddd4898221d14639efa02e3ca65b7fe7d0
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-07-07 11:57:19 +0330

    Disable diagnostics virtual text

--- polish.lua
+++ polish.lua
@@ -1,1 +1,7 @@
 vim.opt.mousescroll = "ver:1,hor:1"
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = false
+  }
+)
