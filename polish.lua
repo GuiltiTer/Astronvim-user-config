17734f34e2747678cdde5c8d7af5ff8af1e9e080
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-07-08 15:26:47 +0330

    Modify diagnostics virtual_text default

--- polish.lua
+++ polish.lua
@@ -1,7 +1,1 @@
 vim.opt.mousescroll = "ver:1,hor:1"
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false
-  }
-)
