be2ced4a3e6fb7b62616b5af93ec9b013a245369
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-07-12 21:55:24 +0330

    Add more plugins

--- plugins.lua
+++ plugins.lua
@@ -2,13 +2,13 @@ local surround = {
   "kylechui/nvim-surround",
   version = "*",
   event = "VeryLazy",
-  config = function() require("nvim-surround").setup() end
+  config = function() require("nvim-surround").setup() end,
 }
 
 local auto_save = {
   "okuuva/auto-save.nvim",
   event = { "InsertLeave", "TextChanged" },
-  opts = require("user.configs.auto-save").opts
+  opts = require("user.configs.auto-save").opts,
 }
 
 local todo_comments = {
@@ -23,16 +23,17 @@ local neoclip = {
   "AckslD/nvim-neoclip.lua",
   config = require("user.configs.neoclip").config,
   event = "BufRead",
-  dependencies = { 'nvim-telescope/telescope.nvim', 'kkharji/sqlite.lua', },
+  dependencies = { "nvim-telescope/telescope.nvim", "kkharji/sqlite.lua" },
 }
 
 local telescope = {
   "nvim-telescope/telescope.nvim",
   cmd = "Telescope",
   dependencies = {
-    { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" } },
-  config = require("plugins.configs.telescope"),
-  opts = require("user.configs.telescope").opts
+    { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" },
+  },
+  config = require "plugins.configs.telescope",
+  opts = require("user.configs.telescope").opts,
 }
 
 local flash = {
@@ -54,9 +55,11 @@ local dracula = { "dracula/vim", name = "dracula", priority = 1000, lazy = false
 local catppuccin = { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false }
 local material = { "marko-cerovac/material.nvim", priority = 1000, lazy = false }
 local oxocarbon = { "nyoom-engineering/oxocarbon.nvim", priority = 1000, lazy = false }
-local harpoon = { "ThePrimeagen/harpoon", name = "harpoon", }
+local harpoon = { "ThePrimeagen/harpoon", name = "harpoon" }
 local undotree = { "mbbill/undotree", name = "undotree", cmd = "UndotreeToggle" }
 local codeium = { "Exafunction/codeium.vim", event = "InsertEnter" }
+local nvim_ghost = { "subnut/nvim-ghost.nvim", lazy = false }
+local fugitive = { "tpope/vim-fugitive", lazy = false }
 
 return {
   alpha,
@@ -80,4 +83,6 @@ return {
   telescope,
   cccpick,
   twilight,
+  nvim_ghost,
+  fugitive,
 }
