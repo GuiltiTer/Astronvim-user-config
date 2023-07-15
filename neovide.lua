957238f006e4685117c8ffe047ab0e0e5fb0ac4c
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-07-15 09:59:33 +0330

    Lower neovide's fps

--- neovide.lua
+++ neovide.lua
@@ -2,4 +2,4 @@ vim.o.guifont = "JetBrainsMono NF:h12"
 vim.o.termguicolors = true
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_theme = "auto"
-vim.g.neovide_refresh_rate = 120
+vim.g.neovide_refresh_rate = 100
