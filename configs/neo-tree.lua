651cd357d0b3e9c68aea77c1a69e519dc3b58b24
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-07-18 11:41:47 +0330

    Fix Neotree close on file_open event

--- configs/neo-tree.lua
+++ configs/neo-tree.lua
@@ -1,6 +1,6 @@
 return {
   opts = function(_, opts)
-    local close_on_open_handler = { event = "file_opened", handler = function(_) require("neo-tree").close_all() end }
+    local close_on_open_handler = { event = "file_opened", handler = function(_) vim.fn.execute "Neotree close" end }
     table.insert(opts.event_handlers, close_on_open_handler)
     return opts
   end,
