a9f72b561d4019cd31211202e9eb2e5af9cd8581
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-07-19 12:11:27 +0330

    Refactor neovide configs

---
+++ neovide/init.lua
@@ -0,0 +1,9 @@
+require "user.neovide.mappings"
+
+Neovide_default_font_size = 12
+Neovide_font_size_adjust_step = 2
+vim.o.guifont = "JetBrainsMono NF:h" .. Neovide_default_font_size
+vim.o.termguicolors = true
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_theme = "auto"
+vim.g.neovide_refresh_rate = 100
--- neovide/mappings.lua
+++ neovide/mappings.lua
@@ -1,11 +1,3 @@
-local default_font_size = 12
-local font_size_adjust_step = 2
-vim.o.guifont = "JetBrainsMono NF:h" .. default_font_size
-vim.o.termguicolors = true
-vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_theme = "auto"
-vim.g.neovide_refresh_rate = 100
-
 local get_font_prop = function()
   local i = string.find(vim.o.guifont, ":")
   local font_family = string.sub(vim.o.guifont, 0, i - 1)
@@ -17,21 +9,21 @@ local set_font = function(font_family, font_size) vim.o.guifont = font_family ..
 
 local inc_font_size = function()
   local font_family, current_size = get_font_prop()
-  local new_size = current_size + font_size_adjust_step
+  local new_size = current_size + Neovide_font_size_adjust_step
   if new_size > 40 then return end
   set_font(font_family, new_size)
 end
 
 local dec_font_size = function()
   local font_family, current_size = get_font_prop()
-  local new_size = current_size - font_size_adjust_step
+  local new_size = current_size - Neovide_font_size_adjust_step
   if new_size < 2 then return end
   set_font(font_family, new_size)
 end
 
 local reset_font_size = function()
   local font_family, _ = get_font_prop()
-  set_font(font_family, default_font_size)
+  set_font(font_family, Neovide_default_font_size)
 end
 
 vim.keymap.set("n", "<D-=>", inc_font_size)
