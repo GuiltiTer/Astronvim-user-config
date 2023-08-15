d1e4ac10dbf1e148e9da572795d934ac7f1ad8a5
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-08-15 11:40:55 +0330

    Move file browser extention load to telescope

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -5,4 +5,8 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" },
   },
   config = require "plugins.configs.telescope",
+  opts = function(_, opts)
+    require("telescope").load_extension "file_browser"
+    return opts
+  end,
 }
