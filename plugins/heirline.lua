d1c9b021c23804edb32688ceb754ca030cc827d8
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-10-28 00:38:51 +0330

    Modify statusline

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -3,13 +3,13 @@ local status = require "astronvim.utils.status"
 local statusline = {
   hl = { fg = "fg", bg = "bg" },
   status.component.mode(),
+  status.component.git_branch(),
   status.component.file_info {
-    file_modified = { padding = { left = 0, right = 0 } },
+    file_modified = { padding = { left = 1, right = 0 }, hl = { fg = "fg", bg = "bg" } },
+    filename = { padding = { left = 0, right = 0 }, hl = { fg = "fg", bg = "bg" } },
     filetype = false,
-    filename = false,
     file_icon = false,
   },
-  status.component.git_branch(),
   status.component.git_diff(),
   status.component.fill(),
   status.component.cmd_info(),
