f02cd45f92820fd63767eb3a6a131e9422b24446
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-11-29 21:01:24 +0330

    Add codeium to cmp

---
+++ plugins/cmp.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-cmp",
+  dependencies = {
+    {
+      "Exafunction/codeium.nvim",
+      cmd = "Codeium",
+      build = ":Codeium Auth",
+      opts = {},
+    },
+  },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    table.insert(opts.sources, 1, {
+      name = "codeium",
+      group_index = 1,
+      priority = 100,
+    })
+  end,
+}
