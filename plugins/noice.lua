70d53ca745ad1e0fcf370f05f29b96e2c5d2be34
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-02-08 15:28:27 +0330

    Modify noice config

--- plugins/noice.lua
+++ plugins/noice.lua
@@ -5,8 +5,25 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
+
+      lsp = {
+        progress = {
+          enabled = false, --disable bottom right progress bar
+        },
+      },
+
       presets = {
         lsp_doc_border = true,
+      },
+
+      routes = {
+        {
+          -- prevent notification burst
+          filter = {
+            event = "msg_show",
+          },
+          opts = { skip = false },
+        },
       },
     },
   },
