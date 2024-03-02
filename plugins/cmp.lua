0a5f4c0d57029db5de29d4ee4f4554d8fc4cb5ef
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-03-03 02:39:35 +0330

    Add cmp-cmdline plugin

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,19 +1,54 @@
+local cmp = require "cmp"
+
+local dependencies = {
+  "hrsh7th/cmp-cmdline",
+  {
+    "Exafunction/codeium.nvim",
+    cmd = "Codeium",
+    build = ":Codeium Auth",
+  },
+}
+
+local apply_cmdline = function()
+  cmp.setup.cmdline("/", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "buffer" },
+    },
+  })
+
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "path" },
+    }, {
+      {
+        name = "cmdline",
+        option = {
+          ignore_cmds = { "Man", "!" },
+        },
+      },
+    }),
+  })
+end
+
 return {
-  "nvim-cmp",
-  dependencies = {
-    {
-      "Exafunction/codeium.nvim",
-      cmd = "Codeium",
-      build = ":Codeium Auth",
-      opts = {},
+  "hrsh7th/nvim-cmp",
+  keys = { ":", "/" },
+  dependencies = dependencies,
+
+  config = function(_, opts)
+    cmp.setup(opts)
+    apply_cmdline()
+  end,
+
+  opts = {
+    sources = {
+      {
+        name = "codeium",
+        group_index = 1,
+        priority = 100,
+      },
     },
   },
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    table.insert(opts.sources, 1, {
-      name = "codeium",
-      group_index = 1,
-      priority = 100,
-    })
-  end,
 }
