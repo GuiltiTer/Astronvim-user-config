14c5c5bb29187141e8d8485c152b113b82330784
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-03-25 23:50:27 +0330

    Modify neotree's config

--- plugins/neo_tree.lua
+++ plugins/neo_tree.lua
@@ -1,12 +1,7 @@
-local mappings = {}
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = function(_, opts)
-    opts.sources = { "filesystem", "git_status" }
-    -- local close_on_open_handler = { event = "file_opened", handler = function(_) vim.fn.execute "Neotree close" end }
-    -- table.insert(opts.event_handlers, close_on_open_handler)
-    opts.window.mappings = vim.tbl_extend("force", opts.window.mappings, mappings)
-    return opts
-  end,
+  opts = {
+    sources = { "filesystem" },
+    source_selector = { winbar = false },
+  },
 }
